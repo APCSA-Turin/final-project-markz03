c409d23c7eae593524a26b3b8f831bcbbbcd3a99
Mark Zhou <149003514+markz03@users.noreply.github.com>
2025-05-22 00:59:59 +0000

    functionality(waka)

--- JavaAPIProject/src/main/java/com/example/Wakamo.java
+++ JavaAPIProject/src/main/java/com/example/Wakamo.java
@@ -7,53 +7,123 @@ import javax.swing.border.Border;
 
 public class Wakamo extends Game{
     JFrame game;
-    JLabel Tlabel;
-    JPanel Tpanel, Bpanel;
+    JLabel textLabel;
+    JLabel timeLabel;
+    JPanel textPanel, boardPanel;
     JButton[] board;
+    ImageIcon moleIcon;
+    ImageIcon holeIcon;
+
+    JButton currMoleTile;
+
+    Random random = new Random();
+    Timer setMoleTimer;
+    Timer timer;
+    int score = 0;
 
     public Wakamo() {
         super();
         game = new JFrame();
-        Tlabel = new JLabel();
-        Tpanel = new JPanel();
-        Bpanel = new JPanel();
+        textLabel = new JLabel();
+        textPanel = new JPanel();
+        boardPanel = new JPanel();
         board = new JButton[9];
+        timeLabel = new JLabel();
 
-        game = new JFrame("Whack-a-mOLE");
-        game.setSize(600, 650);
+        game = new JFrame("Whack-a-Mole");
+        game.setSize(600, 750);
         game.setLayout(new BorderLayout());
         game.setResizable(false);
         game.setLocationRelativeTo(null);
         game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
-        Tlabel.setFont(new Font("Arial", Font.PLAIN, 50));
-        Tlabel.setText("Score: 0");
-        Tlabel.setOpaque(true);
-        Tlabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setFont(new Font("Arial", Font.PLAIN, 50));
+        textLabel.setText("Score: 0");
+        textLabel.setOpaque(true);
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
 
-        Tpanel.setLayout(new BorderLayout());
-        Tpanel.add(Tlabel);
-        game.add(Tpanel, BorderLayout.NORTH);
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        game.add(textPanel, BorderLayout.NORTH);
 
 
-        Bpanel.setLayout(new GridLayout(3,3));
-        game.add(Bpanel);
+        boardPanel.setLayout(new GridLayout(3,3));
+        game.add(boardPanel);
+
+        Image holeImg = new ImageIcon(getClass().getResource("./hole.png")).getImage();
+        holeIcon = new ImageIcon(holeImg.getScaledInstance(200, 200, java.awt.Image.SCALE_SMOOTH));
+        Image moleImg = new ImageIcon(getClass().getResource("./mole.png")).getImage();
+        moleIcon = new ImageIcon(moleImg.getScaledInstance(200, 200, java.awt.Image.SCALE_SMOOTH));
+
+        timeLabel.setFont(new Font("Arial", Font.PLAIN, 50));
+        timeLabel.setOpaque(true);
+        timeLabel.setHorizontalAlignment(JLabel.CENTER);
+        timer = new Timer(1000, new ActionListener() {
+            int remainingSeconds = 30;
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                remainingSeconds--;
+                timeLabel.setText("Time: " + remainingSeconds);
+                if (remainingSeconds <= 0) {
+                    timer.stop();
+                    timeLabel.setText("Time's up!");
+                    System.out.println("TIMES OUT");
+                    for (int i = 0; i < 9; i++) {
+                        board[i].setEnabled(false);
+                    }
+                    setMoleTimer.stop();
+                }
+            }
+        });
+        timer.start();
+        game.add(timeLabel, BorderLayout.SOUTH);
+
+
 
         for (int i = 0; i < 9; i++) {
             JButton hole = new JButton();
             board[i] = hole;
-            Bpanel.add(hole);
+            hole.setFocusable(false);
+            boardPanel.add(hole);
+            hole.setIcon(holeIcon);
+
+            hole.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    JButton hole = (JButton) e.getSource();
+                    if (hole == currMoleTile) {
+                        score+=5;
+                        textLabel.setText("Score: " + Integer.toString(score));
+                    }
+                    
+                }
+            });
+
         }
 
 
 
+
+        setMoleTimer = new Timer(750, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (currMoleTile != null) {
+                    currMoleTile.setIcon(holeIcon);
+                    currMoleTile = null;
+                }
+
+                int num = random.nextInt(9);
+                JButton hole = board[num];
+
+                currMoleTile = hole;
+                currMoleTile.setIcon(moleIcon);
+            }
+        });
+        setMoleTimer.start();
+
+    
+
+
         game.setVisible(true);
-
-
-
-
-
 
     }
 
